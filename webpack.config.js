4d263ec00f5a4a6120fa64d5048404eee0fadf0b
Ilya Ashchepkov <koctep@gmail.com>
2019-12-23 21:18:25 +0700

    Add index.html, add some webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 var path = require('path');
+var htmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolv(__dirname, 'dist')
-    }
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+      new htmlWebPackPlugin({
+        template: 'src/index.html',
+        inject: 'head',
+        hash: true
+      })
+    ]
 };
