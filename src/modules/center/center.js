aabf1036c9dd5d4df20266e554b5afd05cab9d45
Ice power <midonova.anastasiia@gmail.com>
2020-01-29 19:32:01 +0700

    add new modules

---
+++ src/modules/center/center.js
@@ -0,0 +1,31 @@
+﻿import angluar from 'angular';
+
+import './centers.scss';
+
+import template from './centers.html';
+
+class orgsController {
+    constructor() {
+        'ngInject';
+        this.s = [
+            { center_id: 1, name: 'Дионис' },
+            { center_id: 2, name: 'Иля' }
+        ];
+    }
+};
+
+let controller = centersController;
+
+let centersComponent = {
+    restrict: 'E',
+    bindings: {},
+    template,
+    controller,
+    controllerAs: 'vm'
+}
+
+let centersModule = angular.module('centers', [])
+
+    .component('centers', centersComponent);
+
+export default centersModule;
---
+++ src/modules/head/head.js
@@ -0,0 +1,31 @@
+﻿import angluar from 'angular';
+
+import './heads.scss';
+
+import template from './heads.html';
+
+class orgsController {
+    constructor() {
+        'ngInject';
+        this.s = [
+            { head_id: 1, name: 'Анонс' },
+            { head_id: 2, name: 'Расписание' }
+        ];
+    }
+};
+
+let controller = headsController;
+
+let gamesComponent = {
+    restrict: 'E',
+    bindings: {},
+    template,
+    controller,
+    controllerAs: 'vm'
+}
+
+let headsModule = angular.module('heads', [])
+
+    .component('heads', headsComponent);
+
+export default headsModule;
