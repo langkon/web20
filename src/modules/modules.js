8e370affcbf871cfa274193c82b6fc2f705c4d0c
Ice power <midonova.anastasiia@gmail.com>
2020-02-05 19:00:35 +0700

    add module

--- src/modules/modules.js
+++ src/modules/modules.js
@@ -1,12 +1,17 @@
 import './events/events.js';
 import './users/users.js';
+import './users/centers.js';
+import './users/games.js';
+import './users/heads.js';
+import './users/news.js';
+import './users/orgs.js';
 
 angular.module('modules', [
     'events',
     'users',
-    'center',
+    'centers',
     'games',
-    'head',
+    'heads',
     'news',
-    'org'
+    'orgs'
 ]);
